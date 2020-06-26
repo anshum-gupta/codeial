5f83f8f37aa4fba763825b0d452cef6c314fde6e
Anshum Gupta <anshum_it_is@MacBook-Pro.local>
2020-06-27 01:10:14 +0530

    Passport installed

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
+const session = require('express-session');
+const passport = require('passport');
+const passportLocal = require('./config/passport-local-strategy');
 
 app.use(express.urlencoded());
 app.use(cookieParser());
@@ -18,9 +21,23 @@ app.set('layout extractScripts', true);
 
 //use express router
 app.use('/', require('./routes'));
-
 app.set('view engine', 'ejs');
 app.set('views', './views');
+app.use(session({
+    name : 'Codeial',
+    // TODO => change secret before deployment into production
+    secret : 'blahsomething',
+    saveUninitialized : false,
+    resave : false,
+    cookie : {
+        maxAge : (1000 * 60 * 100)
+    }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 app.listen(port, function(err){
     if(err){
         // console.log("Error in running the server : ", err);
