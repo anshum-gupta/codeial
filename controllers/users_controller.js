ca7e7ecc84c60d68169fbe30dcf42ba535a08f82
Anshum Gupta <anshum_it_is@MacBook-Pro.local>
2020-06-26 21:01:35 +0530

    Sign up and sign in page started, User schema created

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -5,3 +5,15 @@ module.exports.profile = function(req, res){
     });
     //  res.end('<h1> User profile </h1>');
 };
+
+module.exports.signUp = function(req, res){
+    return res.render('user_sign_up', {
+        title : "Codeial | Sign Up"
+    });
+};
+
+module.exports.signIn = function(req, res){
+    return res.render('user_sign_in', {
+        title : " Codeial | Sign In"
+    });
+};
