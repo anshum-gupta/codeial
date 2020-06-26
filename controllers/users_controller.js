3e14adc908080d95d5a367e56609192e69445b59
Anshum Gupta <anshum_it_is@MacBook-Pro.local>
2020-06-26 22:15:57 +0530

    Sign Up and Sign In pages basics created plus basic ways for checking validity of data provided

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,4 +1,5 @@
-console.log('inside users contoller');
+// console.log('inside users contoller');
+const User = require('../models/user');
 module.exports.profile = function(req, res){
     return res.render('user_profile', {
         title : "User Profile"
@@ -6,14 +7,43 @@ module.exports.profile = function(req, res){
     //  res.end('<h1> User profile </h1>');
 };
 
+// render the sign up page
 module.exports.signUp = function(req, res){
     return res.render('user_sign_up', {
         title : "Codeial | Sign Up"
     });
 };
 
+
+// render the sign in page
 module.exports.signIn = function(req, res){
     return res.render('user_sign_in', {
         title : " Codeial | Sign In"
     });
 };
+
+//get the sign up data
+module.exports.create = function(req, res){
+    if(req.body.password != req.body.confirm_password){
+        return res.redirect('back');
+    }
+    
+    User.findOne({email : req.body.email}, function(err, user){
+        if(err){
+            console.log('Error in finding user in signing up!');
+            return;
+        }
+        else if(!user){
+            User.create(req.body, function(err, user){
+                if(err){
+                    console.log('Error in finding user in signing up!');
+                    return;
+                }
+                else return res.redirect('/users/sign-in');
+            });
+        }
+        else{
+            return res.redirect('back');
+        }
+    });
+}
