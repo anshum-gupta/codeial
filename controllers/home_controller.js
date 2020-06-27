6910a4ceb3eec0c064e0181974bc76349ce87309
Anshum Gupta <anshum_it_is@MacBook-Pro.local>
2020-06-27 20:07:13 +0530

    Posts with who posted it displayed

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,12 +1,27 @@
 const { readyState } = require("../config/mongoose");
+const Post = require('../models/post');
+const { post } = require("../routes");
 
 module.exports.home = function(req, res){
     // return res.end('<h1>Express is up for Codeial </h1>');
     console.log(req.cookies);
     res.cookie('user_id', 25);
-    return res.render('home', {
-        title : "Codeial Home"
+    
+
+    // Post.find({}, function (err, posts) {
+    //     return res.render('home', {
+    //         title : "Codeial | Home",
+    //         posts : posts
+    //     });
+    // });
+
+    Post.find({}).populate('user').exec( function (err, posts) {
+        return res.render('home', {
+            title : "Codeial | Home",
+            posts : posts
+        });
     });
+    // populate the user of each post
 }
 
 // module.exports.actionName = function(req, res){}
