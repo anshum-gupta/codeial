150f7ed7fb0ebde11796a40ddd914a57076a14fb
Anshum Gupta <anshum_it_is@MacBook-Pro.local>
2020-06-28 14:40:33 +0530

    Deleting of comments option added

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -24,7 +24,7 @@ module.exports.home = function(req, res){
         }
     })
     .exec( function (err, posts) {
-        console.log('posts = > ', posts);
+        // console.log('posts = > ', posts);
         return res.render('home', {
             title : "Codeial | Home",
             posts : posts
